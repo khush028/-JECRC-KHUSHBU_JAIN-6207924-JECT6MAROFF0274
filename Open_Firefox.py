84ad9dc1f6e0c601879b798e337e12ae2a429a6f
khushbu jain <khushbujaink7568@gmail.com>
2026-03-14 13:48:06 +0530

    Delete Open_Firefox.py

--- Open_Firefox.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-opts = webdriver.FirefoxOptions()
-opts.set_preference("detach" , True)
-driver = webdriver.Firefox(options=opts)
-driver.get("https://youtube.com/")
-driver.maximize_window()
-# driver.close()
